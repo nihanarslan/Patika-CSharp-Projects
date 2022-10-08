8cb856be44417b77537c7396d446030487414f70
Nihan Arslan <nihanarslan@outlook.com>
2022-10-08 23:40:51 +0300

    Kolay Seviye projelerden Fibonacchi serisi ortalaması bulma projesi eklendi

---
+++ Kolay_Seviye_Projeler/Console_Average_Calculation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Console_Average_Calculation 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir sayı giriniz: ");
+            bool state = int.TryParse(Console.ReadLine(), out int i);
+            
+            int toplam = CalculateAverage(i);
+
+            Console.WriteLine("\nOrtalama = {0}",(float)toplam/i);
+
+        }
+
+        static int CalculateAverage(int i)
+        {
+            int toplam=0, a=0, b=0, temp = 0;
+            while(i>0)
+            {
+                Console.Write(a +" ");
+                if(a == 0)
+                    b++;
+                toplam += a;
+                temp = b;
+                b = a+b;
+                a = temp;
+                i--;
+            }
+
+            return toplam;
+        }
+    }
+}
