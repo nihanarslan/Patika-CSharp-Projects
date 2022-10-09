3cb86f2c594c87f12d6c46345419fdd4b3129d16
Nihan Arslan <nihanarslan@outlook.com>
2022-10-09 22:36:49 +0300

    Triangle drawing console application has been added

---
+++ Kolay_Seviye_Projeler/Console_Drawing_Triangle/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Ucgen_Cizim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = GetTheLength();
+            
+            DrawTriangle(n);
+        }
+
+        static int GetTheLength()
+        {
+            Console.Write("Bir sayı giriniz: ");
+            return int.Parse(Console.ReadLine());
+        }
+
+        static void DrawTriangle(int n)
+        {
+            for(int i=1; i<= n; i++)
+            {
+                for (int j = n-i; j > 0; j--)
+                {
+                   Console.Write(" ");
+                }
+
+                for (int k = 1; k <= 2*(i-1)+1; k++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
